439d6f391a01f05ccd758f5f985fea255ad73580
Xiaoming Lu <xiaoming@ufl.edu>
2018-02-18 20:58:31 -0500

    adding audio analyzer

--- client/app/factories/listings.client.factory.js
+++ client/app/factories/listings.client.factory.js
@@ -2,23 +2,23 @@ angular.module('listings').factory('Listings', ['$http',
   function($http) {
     var methods = {
       getAll: function() {
-        return $http.get('https://followthebeep.herokuapp.com/api/listings');
+        return $http.get('http://localhost:8080/api/listings');
       },
 
       create: function(listing) {
-        return $http.post('https://followthebeep.herokuapp.com/api/listings', listing);
+        return $http.post('http://localhost:8080/api/listings', listing);
       }, 
 
       read: function(id) {
-        return $http.get('https://followthebeep.herokuapp.com/api/listings/' + id);
+        return $http.get('http://localhost:8080/api/listings/' + id);
       }, 
 
       update: function(id, listing) {
-        return $http.put('https://followthebeep.herokuapp.com/api/listings/' + id, listing);
+        return $http.put('http://localhost:8080/api/listings/' + id, listing);
       }, 
 
       delete: function(id) {
-        return $http.delete('https://followthebeep.herokuapp.com/api/listings/' + id);
+        return $http.delete('http://localhost:8080/api/listings/' + id);
       }
     };
 
