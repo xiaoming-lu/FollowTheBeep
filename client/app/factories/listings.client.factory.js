89739bb9dc138ca63b6fadf8dbc7b331d9b6da6c
Xiaoming Lu <xiaoming@ufl.edu>
2018-02-20 15:34:53 -0500

    minor edit of host

--- client/app/factories/listings.client.factory.js
+++ client/app/factories/listings.client.factory.js
@@ -1,24 +1,30 @@
 angular.module('listings').factory('Listings', ['$http', 
   function($http) {
-      var host =  process.env.HOST || 'http://localhost:8080';
+
+
     var methods = {
       getAll: function() {
+          var host =  process.env.HOST || 'http://localhost:8080';
         return $http.get(host+'/api/listings');
       },
 
       create: function(listing) {
+          var host =  process.env.HOST || 'http://localhost:8080';
         return $http.post(host+'/api/listings', listing);
       }, 
 
       read: function(id) {
+          var host =  process.env.HOST || 'http://localhost:8080';
         return $http.get(host+'/api/listings/' + id);
       }, 
 
       update: function(id, listing) {
+          var host =  process.env.HOST || 'http://localhost:8080';
         return $http.put(host+'/api/listings/' + id, listing);
       }, 
 
       delete: function(id) {
+          var host =  process.env.HOST || 'http://localhost:8080';
         return $http.delete(host+'/api/listings/' + id);
       }
     };
--- server/config/app.js
+++ server/config/app.js
@@ -5,6 +5,6 @@ module.exports.start = function() {
   var app = express.init();
   var port =  process.env.PORT || 8080;
   app.listen(port, function() {
-    console.log('App listening on port', port);
+      console.log('App listening on port', port);
   });
 };
