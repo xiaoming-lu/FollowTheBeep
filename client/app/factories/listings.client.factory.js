738dfa8920dc22d3f9f8b308a910019b2f8c79f0
Xiaoming Lu <xiaoming@ufl.edu>
2018-02-20 15:43:06 -0500

    fixed localhost and remote issue

--- client/app/factories/listings.client.factory.js
+++ client/app/factories/listings.client.factory.js
@@ -4,27 +4,47 @@ angular.module('listings').factory('Listings', ['$http',
 
     var methods = {
       getAll: function() {
-          var host =  process.env.HOST || 'http://localhost:8080';
+          var host;
+          if(location.hostname === "localhost")
+            host = 'http://localhost:8080';
+          else
+              host = 'https://followthebeep.herokuapp.com'
         return $http.get(host+'/api/listings');
       },
 
       create: function(listing) {
-          var host =  process.env.HOST || 'http://localhost:8080';
+          var host;
+          if(location.hostname === "localhost")
+              host = 'http://localhost:8080';
+          else
+              host = 'https://followthebeep.herokuapp.com'
         return $http.post(host+'/api/listings', listing);
       }, 
 
       read: function(id) {
-          var host =  process.env.HOST || 'http://localhost:8080';
+          var host;
+          if(location.hostname === "localhost")
+              host = 'http://localhost:8080';
+          else
+              host = 'https://followthebeep.herokuapp.com'
         return $http.get(host+'/api/listings/' + id);
       }, 
 
       update: function(id, listing) {
-          var host =  process.env.HOST || 'http://localhost:8080';
+          var host;
+          if(location.hostname === "localhost")
+              host = 'http://localhost:8080';
+          else
+              host = 'https://followthebeep.herokuapp.com'
         return $http.put(host+'/api/listings/' + id, listing);
       }, 
 
       delete: function(id) {
-          var host =  process.env.HOST || 'http://localhost:8080';
+          var host;
+          if(location.hostname === "localhost")
+              host = 'http://localhost:8080';
+          else
+              host = 'https://followthebeep.herokuapp.com'
         return $http.delete(host+'/api/listings/' + id);
       }
     };
