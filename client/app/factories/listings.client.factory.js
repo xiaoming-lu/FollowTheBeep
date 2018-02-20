924d3f9eaf86b2ec520c851d109f389913d243fb
Xiaoming Lu <xiaoming@ufl.edu>
2018-02-20 15:45:49 -0500

    .

--- client/app/factories/listings.client.factory.js
+++ client/app/factories/listings.client.factory.js
@@ -1,50 +1,34 @@
 angular.module('listings').factory('Listings', ['$http', 
   function($http) {
-
+      var host;
+      if(location.hostname === "localhost")
+          host = 'http://localhost:8080';
+      else
+          host = 'https://followthebeep.herokuapp.com'
 
     var methods = {
       getAll: function() {
-          var host;
-          if(location.hostname === "localhost")
-            host = 'http://localhost:8080';
-          else
-              host = 'https://followthebeep.herokuapp.com'
+
         return $http.get(host+'/api/listings');
       },
 
       create: function(listing) {
-          var host;
-          if(location.hostname === "localhost")
-              host = 'http://localhost:8080';
-          else
-              host = 'https://followthebeep.herokuapp.com'
+
         return $http.post(host+'/api/listings', listing);
       }, 
 
       read: function(id) {
-          var host;
-          if(location.hostname === "localhost")
-              host = 'http://localhost:8080';
-          else
-              host = 'https://followthebeep.herokuapp.com'
+
         return $http.get(host+'/api/listings/' + id);
       }, 
 
       update: function(id, listing) {
-          var host;
-          if(location.hostname === "localhost")
-              host = 'http://localhost:8080';
-          else
-              host = 'https://followthebeep.herokuapp.com'
+
         return $http.put(host+'/api/listings/' + id, listing);
       }, 
 
       delete: function(id) {
-          var host;
-          if(location.hostname === "localhost")
-              host = 'http://localhost:8080';
-          else
-              host = 'https://followthebeep.herokuapp.com'
+   
         return $http.delete(host+'/api/listings/' + id);
       }
     };
